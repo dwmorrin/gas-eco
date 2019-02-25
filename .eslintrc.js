c0e13700b49ae42d5b85080a3dfde7d2ce540201
David Morrin <dm187@nyu.edu>
2019-02-25 14:44:47 -0500

    Renamed files to have forward slashes.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "plugins": [
+        "googelappsscript"
+    ],
+    "env": {
+        "googleappsscript/googleappsscript": true,
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
