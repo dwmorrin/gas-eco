1fd5ab842e3e39ccfe99d013022411d456c2fb79
David Morrin <dm187@nyu.edu>
2019-03-07 12:53:31 -0500

    Update file descriptions to match new file names

--- README.js
+++ README.js
@@ -1,42 +1,35 @@
 /**
- * EQUIPMENT CHECK-OUT
+ * EQUIPMENT CHECK OUT
+ * @author David Morrin <dm187@nyu.edu>
  *
  * This is an inventory management program.
  * Item barcodes and IDs are scanned to associate items with students.
  *
  * Code organization:
- *   Because the apps script project has no file system, the file name
- *   must reflect the file hierarchy using camelCase.
- *   i.e. read jsAppChanges.html as js/App/Changes.html
- *   
  *   CLIENT SIDE JAVASCRIPT:
- *
- *     jsApp: Main app object.
- *       jsApp<OBJ>: some OBJ inside of the main app object
+ *     js/app: Main app object.
+ *       js/app/<object>: some object inside of the main app object
  *     
- *     jsDisplay: presentation routines (views)
- *     jsInit: Init routine
- *     jsLogic: client side controller logic
- *     jsUtility: DOM, date, and form helper functions
+ *     js/Display: presentation routines (views)
+ *     js/Init: Init routine
+ *     js/Logic: client side controller logic
+ *     js/Utility: DOM, date, and form helper functions
  *     
  *  HTML:
- *     index: main HTML template
- *     cssStyle: the stylesheet
- *     page<PageName>: straight HTML files for templating
+ *     html/index: main HTML template
+ *     html/<PageName>: straight HTML files for templating
+ *     css/Style: the stylesheet
  *
  *  SERVER SIDE JAVASCRIPT:
- *     These files start with an uppercase letter so they sort nicely in the GAS
- *     web editor.
- *
  *     MODELS:
- *       Booking
- *       Form
- *       Item
- *       Note
- *       Student
+ *       gs/Booking
+ *       gs/Form
+ *       gs/Item
+ *       gs/Note
+ *       gs/Student
  *
- *     LOGIC:
- *       Main: Just in case we switch to SQL database, this is implementation agnostic
+ *     CONTROLLERS:
+ *       Main: implementation agnostic router of requests, responses
  *       Sheets: the Sheets implementation specific code
  *       Demo: the demo implementation specific code
  */
