42071fbd1c954cebf6480a5369ecb7da5d838a86
David Morrin <dwmorrin@gmail.com>
2019-05-22 11:32:57 -0400

    fix: update doc type formatting

--- gs/Student.js
+++ gs/Student.js
@@ -1,13 +1,13 @@
 /* exported Student_ */
 function Student_(id) {
-  this.id = id;         // string corresponding to ID barcode or RFID
-  this.checkIn = null;  // string formatted date
-  this.checkOut = null; // string formatted date
-  this.contact;         // string phone number
-  this.name;            // string
-  this.netId;           // string
-  this.signatureOnFile; // boolean
-  this.left = false;            // boolean, states whether or not the student left without checking out
+  this.id = id;         // {string} corresponding to ID barcode or RFID
+  this.checkIn = null;  // {string} formatted date
+  this.checkOut = null; // {string} formatted date
+  this.contact;         // {string} phone number
+  this.name;            // {string}
+  this.netId;           // {string}
+  this.signatureOnFile; // {bool}
+  this.left = false;    // {bool} true if student left without checking out
 
   this.getCheckIn = function() { return this.checkIn; };
   this.getCheckOut = function() { return this.checkOut; };
@@ -22,5 +22,8 @@ function Student_(id) {
   this.setContact = function(string) { this.contact = string; return this; };
   this.setName = function(name) { this.name = name; return this; };
   this.setNetId = function(netId) { this.netId = netId; return this; };
-  this.setSignatureOnFile = function(bool) { this.signatureOnFile = Boolean(bool); return this; };
+  this.setSignatureOnFile = function(bool) {
+    this.signatureOnFile = Boolean(bool);
+    return this;
+  };
 }
